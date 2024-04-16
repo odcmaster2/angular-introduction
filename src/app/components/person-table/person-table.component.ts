0cd8d2fe1c78027fa8696e2424a6c4f95e34e99b
odcmaster2 <odcmaster2@gmail.com>
2024-04-16 16:08:00 +0300

    angular material

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
-import { Person } from 'src/app/shared/interfaces/person';
+import { EPerson, Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
   standalone: true,
   imports: [],
   templateUrl: './person-table.component.html',
-  styleUrl: './person-table.component.css'
+  styleUrl: './person-table.component.css',
 })
 export class PersonTableComponent {
-  @Input() person: Person | undefined;
+  @Input() person:  Person | EPerson | undefined;
+
+  isPerson() {
+    return this.person && 'address' in this.person;
+    
+  }
+
+  isEPerson() {
+    return this.person && 'education' in this.person;
+  }
+
+  getPersonDetails(person: Person | EPerson | undefined): String {
+    if (!person) {
+      return '';
+    } else if ('address' in person) {
+      return person.address;
+    } else if ('education' in person) {
+      return person.education;
+    } else {
+      return '';
+    }
+  }
 }
