d9f2cb6366dd3786348d7cbc66988c1815512979
odcmaster2 <odcmaster2@gmail.com>
2024-04-18 15:25:34 +0300

    reactive forms

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -16,6 +16,7 @@ export class ListGroupMenuComponent {
     {text: 'Event-bind-example', routerLink: 'event-bind-example'},
     {text: 'Simple-datatable-example', routerLink: 'simple-datatable-example'},
     {text: 'Component-output-example', routerLink: 'component-output-example'},
-    {text: 'Template-driven-form-example', routerLink: 'template-driven-form-example'}
+    {text: 'Template-driven-form-example', routerLink: 'template-driven-form-example'},
+    {text: 'Reactive-form-example', routerLink: 'reactive-form-example'}
   ]
 }
---
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
+import { EPerson } from 'src/app/shared/interfaces/person';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+
+@Component({
+  selector: 'app-reactive-form-example',
+  standalone: true,
+  imports: [ EpersonReactiveFormComponent,
+    PersonTableComponent,
+    SimpleDatatableComponent,],
+  templateUrl: './reactive-form-example.component.html',
+  styleUrl: './reactive-form-example.component.css'
+})
+export class ReactiveFormExampleComponent {
+  currentPerson: EPerson | undefined;
+  persons: EPerson[] = [];
+
+  onPerson(person: EPerson) {
+    this.currentPerson = person;
+    this.persons.push(person);
+  }
+
+}
